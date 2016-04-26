e1ce112f7f6eccf43ae739572a23156f5eb13add
Florian Jourda <florian.jourda@gmail.com>
2016-04-26 16:26:31 +0200

    Added missing files

---
+++ retro/__init__.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
+gloss = Blueprint('gloss', __name__)
+db = SQLAlchemy()
+
+def create_app(environ):
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = environ['DATABASE_URL']
+    app.config['DATABASE_URL'] = environ['DATABASE_URL']
+    app.config['SLACK_TOKEN'] = environ['SLACK_TOKEN']
+    app.config['SLACK_WEBHOOK_URL'] = environ['SLACK_WEBHOOK_URL']
+
+    db.init_app(app)
+
+    app.register_blueprint(gloss)
+    return app
+
+from . import views, errors
---
+++ retro/models.py
@@ -0,0 +1,63 @@
+from . import db
+from datetime import datetime
+
+class Sprint(db.Model):
+    ''' Scrum Sprint (Sprint #1, Sprint #2 etc...)
+    '''
+    __tablename__ = 'sprints'
+    # Columns
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.Unicode())
+    creation_date = db.Column(db.DateTime(), default=datetime.utcnow)
+
+    def __repr__(self):
+        return 'Sprint {}, started on {}'.format(self.id, self.creation_date.date())
+
+    @classmethod
+    def get_current_sprint(cls, user_name):
+        ''' Return lasted sprint.
+        '''
+        sprint = Sprint.query.order_by(Sprint.id.desc()).first()
+        if not sprint:
+            # If no sprint has been started yet, we create one
+            sprint = cls.create_new_sprint(user_name)
+        return sprint
+
+    @classmethod
+    def create_new_sprint(cls, user_name):
+        sprint = Sprint(user_name=user_name)
+        db.session.add(sprint)
+        db.session.commit()
+        return sprint
+
+class RetrospectiveItem(db.Model):
+    ''' Item logged for a sprint.
+
+    Example:
+    "Good: Eva joined the team!"
+    "Bad: github was down"
+    '''
+    __tablename__ = 'retrospective_items'
+    # Columns
+    id = db.Column(db.Integer, primary_key=True)
+    sprint_id = db.Column(db.Integer, index=True)
+    category = db.Column(db.Unicode(), index=True)
+    text = db.Column(db.Unicode())
+    user_name = db.Column(db.Unicode())
+    creation_date = db.Column(db.DateTime(), default=datetime.utcnow)
+
+    def __repr__(self):
+        return '{}: {}'.format(self.category.capitalize(), self.text)
+
+    @classmethod
+    def get_retrospective_items_for_sprint(cls, sprint):
+        ''' Return a dict of retrospective items grouped by categoty
+
+        {
+            'good': [item1, item2],
+            'bad': [item3, item4],
+            'try': [item5],
+        }
+        '''
+        items = RetrospectiveItem.query.filter(RetrospectiveItem.sprint_id == sprint.id)
+        return items
