e8b4f893c9fdc6eaa7c04e253dd299d6d1fb8c80
Pascal Corpet <pcorpet@users.noreply.github.com>
2018-06-26 13:29:56 +0200

    Add tests for the basic retro commands. (#14)

--- slack_retro_bot_to_airtable_test.py
+++ slack_retro_bot_to_airtable_test.py
@@ -22,6 +22,7 @@ class TestBot(unittest.TestCase):
 
         self.app = slack_retro_bot_to_airtable.app.test_client()
 
+        airtablemock.clear()
         self.airtable_client = airtablemock.Airtable()
         patcher = mock.patch(
             slack_retro_bot_to_airtable.__name__ + '._AIRTABLE_CLIENT',
@@ -56,60 +57,57 @@ class TestBot(unittest.TestCase):
     #     robo_response = self.post_command(text='')
     #     self.assertEqual(robo_response.status_code, 200)
 
-    # def test_set_retrospective_item_with_good_command(self):
-    #     return self._test_set_retrospective_item('good', 'The coffee was great',
-    #                                              is_using_direct_command=True)
+    def test_set_retrospective_item_with_good_command(self):
+        return self._test_set_retrospective_item(
+            'good', 'good', 'The coffee was great', is_using_direct_command=True)
 
-    # def test_set_retrospective_item_with_bad_command(self):
-    #     return self._test_set_retrospective_item('bad', 'The coffee was bad',
-    #                                              is_using_direct_command=True)
+    def test_set_retrospective_item_with_bad_command(self):
+        return self._test_set_retrospective_item(
+            'bad', 'danger', 'The coffee was bad', is_using_direct_command=True)
 
     # def test_set_retrospective_item_with_try_command(self):
-    #     return self._test_set_retrospective_item('try', 'Make more coffee',
-    #                                              is_using_direct_command=True)
+    #     return self._test_set_retrospective_item(
+    #         'try', 'warning', 'Make more coffee', is_using_direct_command=True)
 
-    # def test_set_retrospective_item_with_retrospective_good_command(self):
-    #     return self._test_set_retrospective_item('good', 'The tea was great',
-    #                                              is_using_direct_command=False)
+    def test_set_retrospective_item_with_retrospective_good_command(self):
+        return self._test_set_retrospective_item(
+            'good', 'good', 'The tea was great', is_using_direct_command=False)
 
-    # def test_set_retrospective_item_with_retrospective_bad_command(self):
-    #     return self._test_set_retrospective_item('bad', 'The tea was bad',
-    #                                              is_using_direct_command=False)
+    def test_set_retrospective_item_with_retrospective_bad_command(self):
+        return self._test_set_retrospective_item(
+            'bad', 'danger', 'The tea was bad', is_using_direct_command=False)
 
     # def test_set_retrospective_item_with_retrospective_try_command(self):
-    #     return self._test_set_retrospective_item('try', 'Make more tea',
-    #                                              is_using_direct_command=False)
+    #     return self._test_set_retrospective_item(
+    #         'try', 'warning', 'Make more tea', is_using_direct_command=False)
 
-    # def _test_set_retrospective_item(self, category, text, is_using_direct_command):
-    #     """ A retrospective item set via a POST is recorded in the database.
-    #     """
-    #     if is_using_direct_command:
-    #         # '/good Bla bla'
-    #         robo_response = self.post_command(
-    #             text=text, slash_command=category)
-    #     else:
-    #         # '/retrospective good Bla bla'
-    #         robo_response = self.post_command(text='{} {}'.format(category, text),
-    #                                           slash_command='retro')
+    def _test_set_retrospective_item(self, category, color, text, is_using_direct_command):
+        """ A retrospective item set via a POST is recorded in the database."""
+        if is_using_direct_command:
+            # '/good Bla bla'
+            robo_response = self._post_command(text=text, slash_command=category)
+        else:
+            # '/retrospective good Bla bla'
+            robo_response = self._post_command(
+                text='{} {}'.format(category, text), slash_command='retro')
 
-    #     date = self._get_sprint_date()
-    #     expected_colors_by_category = {
-    #         'good': 'good', 'bad': 'danger', 'try': 'warning'}
-    #     expected_color = expected_colors_by_category[category]
-    #     self.assertEqual(
-    #         robo_response.data,
-    #         '{{"text": "New retrospective item for *Sprint 1, started on {}*:", '.format(date) +
-    #         '"response_type": "in_channel", "attachments": [' +
-    #         '{{"color": "{}", "text": "{}", "title": "{}"}}]}}'.format(
-    #             expected_color, text, category.capitalize())
-    #     )
-    #     filters = (RetrospectiveItem.category == category,
-    #                RetrospectiveItem.text == text)
-    #     # retrospective_item_check = self.db.session.query(
-    #         # RetrospectiveItem).filter(*filters).first()
-    #     # self.assertIsNotNone(retrospective_item_check)
-    #     # self.assertEqual(retrospective_item_check.category, category)
-    #     # self.assertEqual(retrospective_item_check.text, text)
+        self.assertEqual(
+            {
+                'text': 'New retrospective item:',
+                'response_type': 'in_channel',
+                'attachments': [
+                    {'title': category.capitalize(), 'color': color},
+                    {'color': color, 'text': text},
+                ],
+            },
+            robo_response.json,
+        )
+        items = self.airtable_client.get('Items', view='Current View')
+        self.assertEqual(1, len(items['records']), msg=items)
+        item = items['records'][0]['fields']
+        self.assertEqual(category, item['Category'])
+        self.assertEqual('retroman', item['Creator'])
+        self.assertEqual(text, item['Object'])
 
     def test_list(self):
         """ Test getting the list of all items with POST."""
